477354a8efab6af1b9861d0bf8f9df4fe1882a24
Deenank Sharma <146727112+DeenankSharma@users.noreply.github.com>
2024-06-10 01:15:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import  dotenv from 'dotenv'
 dotenv.config()
 
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT;
 
 const db = new pg.Client({
   user: process.env.DB_USER,
